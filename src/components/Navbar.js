8e9adb50b2a1f4e0baf6b2f10afe0e7f949f82d3
IsoCryptoManiac <isoner.svk@protonmail.com>
2022-01-11 21:50:59 +0100

    upravil som navbar na flexbox plus treba pridelit spravny position navbaru + CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from "react";
+import "./Navbar.css";
 
-export default function Navbar(props){
-    return(
-        <div>
-            {props.data.map(data => <a href={props.data}>{props.data}</a>)}
-        </div>
-    );
+export default function Navbar(props) {
+  const Links = props.data.map((data) => (
+    <a className="links" /*href={data}*/>{data}</a>
+  ));
+  return <div className="navigation">{Links}</div>;
 }

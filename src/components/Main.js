309dd3385f6d372868486631dd7bd11d0945f51f
IsoCryptoManiac <isoner.svk@protonmail.com>
2022-01-17 19:19:12 +0100

    pridal som sekciu portfolio,skills,contact

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,24 @@ import "./Main.css";
 export default function Main(){
     return(
         <div className="section">
-        <p>Hello</p>
+            <div className="portfolio">
+                <h3>Portfolio</h3>
+                <div className="portfolio-grid">
+
+                </div>
+            </div>
+            <div className="skills">
+                <h3>Skills</h3>
+                <div className="skills-list">
+                    
+                </div>
+            </div>
+            <div className="contact">
+                <h3>Contact</h3>
+                <div className="contact-text">
+                    
+                </div>
+            </div>
       </div>
     )
 }

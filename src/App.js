093810896187d3e77b1d3b0db9a4862c51ed8bea
IsoCryptoManiac <isoner.svk@protonmail.com>
2022-01-24 11:50:54 +0100

    Nahral som na cloud dalsiu verziu

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import "./App.css";
 import Header from "./components/Header";
 import Particles from "react-tsparticles";
 import Navbar from "./components/Navbar";
-import Main from "./components/Main"
+import Main from "./components/Main";
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
-const data = ["Home","Portfolio","Skills","Contact",];
+const data = ["Home", "Portfolio", "Skills", "Contact"];
 
 function App() {
   const particlesInit = (main) => {
@@ -16,122 +17,122 @@ function App() {
   return (
     <div className="App">
       <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-          background:{
-            color:{
-              value:"#001429",
-            }
-          },
-        fpsLimit: 60,
-        interactivity: {
-          detectOn: "canvas",
-    events: {
-      onHover: {
-        enable: true,
-        mode: "connect"
-      },
-      onClick: {
-        enable: false,
-        mode: "push"
-      },
-      resize: true
-          },
-          modes: {
-            grab: {
-              distance: 400,
-              lineLinked: {
-                opacity: 1
-              }
-            },
-            bubble: {
-              distance: 400,
-              size: 40,
-              duration: 2,
-              opacity: 8,
-              speed: 3
-            },
-            connect: {},
-            repulse: {
-              distance: 200,
-              duration: 0.4
-            },
-            push: {
-              particles_nb: 4
-            },
-            remove: {
-              particles_nb: 2
-            }
-          },
-        },
-        particles: {
-          number: {
-            value: 80,
-            density: {
-              enable: true,
-              area: 800,
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#001429",
             },
           },
-          color: {
-            value: ["#BD10E0", "#B8E986", "#50E3C2", "#FFD300", "#E86363"],
+          fpsLimit: 60,
+          interactivity: {
+            detectOn: "canvas",
+            events: {
+              onHover: {
+                enable: true,
+                mode: "connect",
+              },
+              onClick: {
+                enable: false,
+                mode: "push",
+              },
+              resize: true,
+            },
+            modes: {
+              grab: {
+                distance: 400,
+                lineLinked: {
+                  opacity: 1,
+                },
+              },
+              bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 8,
+                speed: 3,
+              },
+              connect: {},
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+              push: {
+                particles_nb: 4,
+              },
+              remove: {
+                particles_nb: 2,
+              },
+            },
           },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 0,
-              color: "#b6b2b2"
-            }
-          },
-          opacity: {
-            value: 0.5211089197812949,
-            random: false,
-            animation: {
+          particles: {
+            number: {
+              value: 80,
+              density: {
+                enable: true,
+                area: 800,
+              },
+            },
+            color: {
+              value: ["#BD10E0", "#B8E986", "#50E3C2", "#FFD300", "#E86363"],
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0,
+                color: "#b6b2b2",
+              },
+            },
+            opacity: {
+              value: 0.5211089197812949,
+              random: false,
+              animation: {
+                enable: true,
+                speed: 1,
+                minimumValue: 0.1,
+                sync: false,
+              },
+            },
+            size: {
+              value: 8.017060304327615,
+              random: true,
+              animation: {
+                enable: true,
+                speed: 12.181158184520175,
+                minimumValue: 0.1,
+                sync: false,
+              },
+            },
+            lineLinked: {
+              enable: false,
+              distance: 150,
+              color: "#c8c8c8",
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
               enable: true,
               speed: 1,
-              minimumValue: 0.1,
-              sync: false
-            }
+              direction: "none",
+              random: false,
+              straight: false,
+              outMode: "bounce",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+              },
+            },
           },
-          size: {
-            value: 8.017060304327615,
-            random: true,
-            animation: {
-              enable: true,
-              speed: 12.181158184520175,
-              minimumValue: 0.1,
-              sync: false
-            }
-          },
-          lineLinked: {
-            enable: false,
-            distance: 150,
-            color: "#c8c8c8",
-            opacity: 0.4,
-            width: 1
-          },
-          move: {
-            enable: true,
-            speed: 1,
-            direction: "none",
-            random: false,
-            straight: false,
-            outMode: "bounce",
-            bounce: false,
-            attract: {
-              enable: false,
-              rotateX: 600,
-              rotateY: 1200
-            }
-          },
-        },
-        detectRetina: true,
-      }}
-    />
-      <Header/>
-      <Main/>
-      <Navbar data={data}/>
+          detectRetina: true,
+        }}
+      />
+      <Header />
+      <Main />
+      <Navbar data={data} />
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,41 @@
 import React from "react";
 import "./Main.css";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
-export default function Main(){
-    return(
-        <div className="section">
-            <div className="portfolio">
-                <h3>Portfolio</h3>
-                <div className="portfolio-grid">
-
-                </div>
-            </div>
-            <div className="skills">
-                <h3>Skills</h3>
-                <div className="skills-list">
-                    
-                </div>
-            </div>
-            <div className="contact">
-                <h3>Contact</h3>
-                <div className="contact-text">
-                    
-                </div>
-            </div>
+export default function Main() {
+  return (
+    <div className="section">
+      <div className="portfolio">
+        <h2>Portfolio</h2>
+        <div className="portfolio-grid"></div>
       </div>
-    )
+      <div className="skills">
+        <h2>Skills</h2>
+        <div className="skills-list"></div>
+      </div>
+      <div className="contact">
+        <h2>Contact</h2>
+        <div className="contact-text">
+          <p>
+            Feel free to contact me at{" "}
+            <strong>
+              <a>mtjkapral@protonmail.com</a>
+            </strong>
+            .
+          </p>
+          <p>
+            For viewing my source code from my portfolio projects you can also
+            checkout my GitHub{" "}
+            <strong>
+              <a>
+                <i class="fab fa-github-square"></i>@Nexus
+              </a>
+            </strong>
+            .
+          </p>
+          <p>You can also find me on social networks:</p>
+        </div>
+      </div>
+    </div>
+  );
 }

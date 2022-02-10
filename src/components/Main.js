98c29aa26e7de5ce4b9c1e739f3a4f33879d7c99
IsoCryptoManiac <isoner.svk@protonmail.com>
2022-02-10 23:13:55 +0100

    Pokracovanie v upravach dizajnu a obsahu portfolia

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,14 +4,20 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 
 export default function Main(props) {
   return (
-    <div id={props.id} className="section">
-      <div id="skills" className="portfolio">
+    <div className="section">
+      <div id={props.id} className="portfolio">
         <h2>Portfolio</h2>
-        <div className="portfolio-grid"></div>
+        <div className="portfolio-grid"><p><u>No projects created so far</u></p></div>
       </div>
       <div id="skills" className="skills">
         <h2>Skills</h2>
-        <div className="skills-list"></div>
+        <div className="skills-list">
+          <p>
+            <strong style={{color: "red"}}>Front-end:</strong> HTML, CSS, Javascript, React
+          </p>
+         <p><strong style={{color: "green"}}>Back-end:</strong> Node.js, Express.js, PHP</p>
+         <p><strong style={{color: "blue"}}>Tools:</strong> Photoshop, Premiere, Ilustrator, Git, ffmpeg</p> 
+        </div>
       </div>
       <div id="contact" className="contact">
         <h2>Contact</h2>
@@ -27,8 +33,7 @@ export default function Main(props) {
             For viewing my source code from my portfolio projects you can also
             checkout my GitHub{" "}
             <strong>
-              <a>
-                <i class="fab fa-github-square"></i>@Nexus
+              <a>@Nexus
               </a>
             </strong>
             .

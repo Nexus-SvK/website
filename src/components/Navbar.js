bfecc415685918e6370b2c843cd7b4439e6e994a
IsoCryptoManiac <isoner.svk@protonmail.com>
2022-02-13 17:43:13 +0100

    save pre istotu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,25 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Navbar.css";
 import { Link } from "react-scroll";
+import Toggle from "./Toggle";
+import { render } from "@testing-library/react";
+import { useState } from "react/cjs/react.production.min";
 
 export default function Navbar(props) {
-  const Links = props.data.map((data) => (
-    <Link
-      to={data.id}
-      className="links"
-      key={data}
-      smooth={true}
-      duration={1000}
-    >
-      {data.label}
-    </Link>
-  ));
-  return <div className="navigation">{Links}</div>;
-}
+let links = props.data.map((data) => <Link
+  to={data.id}
+  className="links"
+  key={data}
+  smooth={true}
+  duration={1000}
+>
+  {data.label}
+</Link>)
+
+    return (
+      <div id="nav" className="navigation">
+        <Toggle />
+        {links}
+      </div>
+    );
+  }
